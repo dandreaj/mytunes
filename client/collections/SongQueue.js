a4ad195f8777c87e8c145427da480b99b8bc597a
Jessica D'Andrea <jessica.dandrea@pwc.com>
2017-06-29 22:34:45 -0500

    Completed Song Model, Song View, and majority of Song Queue tests

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -4,6 +4,36 @@ var SongQueue = Backbone.Collection.extend({
   model: SongModel,
 
   initialize: function() {
+    this.on('add', this.enqueue, this);
+    this.on('ended', this.ended, this);
+    this.on('dequeue', this.dequeue, this);
+  },
+
+  enqueue: function() {
+    if (this.length === 1) {
+      this.playFirst();
+    }
+
+  },
+
+  dequeue: function(song) {
+
+    this.remove(song);
+
+  },
+
+  ended: function() {
+
+    this.dequeue(this.at(0));
+
+    if (this.length > 0) {
+      this.playFirst();
+    }
+
+  },
+  //
+  playFirst: function() {
+    this.at(0).play();
   }
 
 });
